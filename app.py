2d26c48bf452590fab1709d714605bbcbcbc9db6
MatBylin <bylinsk@gmail.com>
2019-03-07 11:22:48 +0100

    Add Integration Tests

---
+++ app.py
@@ -0,0 +1,12 @@
+blogs = dict()
+
+def menu():
+    # Show the user the available blog's
+    # Let user make a choice
+    # Do something with that choice
+    # Exit
+    print_blogs()
+
+def print_blogs():
+    # Print the available blog's
+    print('Blogs')
--- blog.py
+++ blog.py
@@ -1,5 +1,6 @@
 from post import Post
 
+
 class Blog:
     def __init__(self, title, author):
         self.title = title
@@ -7,11 +8,17 @@ class Blog:
         self.posts = []
 
     def __repr__(self):
-        return '{} by {} ({} post{})'.format(self.title, self.author, len(self.posts),
+        return '{} by {} ({} post{})'.format(self.title,
+                                             self.author,
+                                             len(self.posts),
                                              's' if len(self.posts) != 1 else '')
 
     def create_post(self, title, content):
         self.posts.append(Post(title, content))
 
     def json(self):
-        pass
+        return {
+            'title': self.title,
+            'author': self.author,
+            'posts': [post.json() for post in self.posts]
+        }
