1b1eef2c5485031069302d791eac7af47ff4b357
MatBylin <bylinsk@gmail.com>
2019-03-07 14:50:15 +0100

    Add SetUp Method

--- tests/system/app_test.py
+++ tests/system/app_test.py
@@ -6,6 +6,10 @@ from post import Post
 
 
 class AppTest(TestCase):
+    def setUp(self):
+        blog = Blog('Test', 'Test Author')
+        app.blogs = {'Test': blog}
+
     def test_menu_prints_prompt(self):
         with patch('builtins.input', return_value='q') as mocked_print:
             app.menu()
@@ -18,9 +22,6 @@ class AppTest(TestCase):
                 mocked_print_blogs.assert_called()
 
     def test_print_blogs(self):
-        blog = Blog('Test', 'Test Author')
-        app.blogs = {'Test': blog}
-
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
@@ -33,18 +34,15 @@ class AppTest(TestCase):
             self.assertIsNotNone(app.blogs.get('Test'))
 
     def test_ask_read_blog(self):
-        blog = Blog('Test', 'Test Author')
-        app.blogs = {'Test': blog}
         with patch('builtins.input', return_value='Test'):
             with patch('app.print_posts') as mocked_print_posts:
                 app.ask_read_blog()
 
-                mocked_print_posts.assert_called_with(blog)
+                mocked_print_posts.assert_called_with(app.blogs['Test'])
 
     def test_print_posts(self):
-        blog = Blog('Test', 'Test Author')
+        blog = app.blogs['Test']
         blog.create_post('Test Post', 'Test Content')
-        app.blogs = {'Test': blog}
 
         with patch('app.print_post') as mocked_print_post:
             app.print_posts(blog)
@@ -60,13 +58,10 @@ class AppTest(TestCase):
             mocked_print.assert_called_with(expected_print)
 
     def test_ask_create_post(self):
-        blog = Blog('Test', 'Test Author')
-        app.blogs ={'Test': blog}
-
         with patch('builtins.input') as mocked_input:
             mocked_input.side_effect = ('Test', 'Test Title', 'Test Content')
 
             app.ask_create_post()
 
-            self.assertEqual(blog.posts[0].title, 'Test Title')
-            self.assertEqual(blog.posts[0].content, 'Test Content')
+            self.assertEqual(app.blogs['Test'].posts[0].title, 'Test Title')
+            self.assertEqual(app.blogs['Test'].posts[0].content, 'Test Content')
